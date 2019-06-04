a3d3a19d8c325e65c3543d5f41ed78bd3d2e8c9b
Breaker-93 <1094143240@qq.com>
2019-06-04 11:02:06 +0800

    添加json-server模拟数据的配置。

---
+++ src/assets/js/jsonServer/index.js
@@ -0,0 +1,17 @@
+const path = require("path")
+// 引入：json-server模块
+const jsonserver = require("json-server")
+// 用jsonserver创建服务对象
+const jserver = jsonserver.create()
+// 创建路由
+const jrouter = jsonserver.router(path.join(__dirname, "json/db.json")) 
+// 定义中间件
+const middlewares = jsonserver.defaults()
+// nodejs的思路，每次请求服务器是，都会执行middlewares中间件
+jserver.use(middlewares)
+// 路由，会根据请求，找对应的数据，如：books或者readers
+jserver.use(jrouter)
+// 启动服务器
+jserver.listen(3333, () => {
+  console.log("json-server running on 3333")
+})
